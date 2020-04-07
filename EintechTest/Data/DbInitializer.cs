39d8f24db09e8e7da94359679175291225d05fa1
digi-chris <chris@barnard.tech>
2020-04-07 12:12:33 +0100

    Set up DbInitializer

---
+++ EintechTest/Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EintechTest.Models;
+
+namespace EintechTest.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DataContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
