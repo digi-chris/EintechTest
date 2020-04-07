9aca3bbe0031f044199f7efe98bdb96391f47e10
digi-chris <chris@barnard.tech>
2020-04-07 12:07:22 +0100

    Basic DataContext

---
+++ EintechTest/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EintechTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EintechTest.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
