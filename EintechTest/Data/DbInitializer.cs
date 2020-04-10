2ad86662b3456bf10cfdf7f2b74a3b6300b50ba8
digi-chris <chris@barnard.tech>
2020-04-10 12:07:49 +0100

    Populate database

--- EintechTest/Data/DbInitializer.cs
+++ EintechTest/Data/DbInitializer.cs
@@ -11,6 +11,23 @@ namespace EintechTest.Data
         public static void Initialize(DataContext context)
         {
             context.Database.EnsureCreated();
+
+            if (!context.Groups.Any())
+            {
+                context.Groups.Add(new Group { Name = "Red Dwarf" });
+                context.Groups.Add(new Group { Name = "Heart of Gold" });
+                context.SaveChanges();
+            }
+
+            if (!context.People.Any())
+            {
+                context.People.Add(new Person { FirstName = "David", MiddleName = "Dave", LastName = "Lister" });
+                context.People.Add(new Person { FirstName = "Arnold", MiddleName = "Judas", LastName = "Rimmer" });
+                context.People.Add(new Person { FirstName = "Arthur", MiddleName = "Philip", LastName = "Dent" });
+                context.People.Add(new Person { FirstName = "Ford", MiddleName = "", LastName = "Prefect" });
+                context.People.Add(new Person { FirstName = "Zaphod", MiddleName = "", LastName = "Beeblebrox" });
+                context.SaveChanges();
+            }
         }
     }
 }
