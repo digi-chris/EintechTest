097b6bc913ab8c94e75053f333b25cae17a4107d
digi-chris <chris@barnard.tech>
2020-04-10 15:17:08 +0100

    Initial tests setup

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,16 +1,49 @@
 using System;
 using Xunit;
-using Moq;
+using EintechTest.Models;
 using EintechTest.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void AddPerson()
+        public async void AddGroup()
         {
-            
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+
+                DbContextOptions<DataContext> options = new DbContextOptionsBuilder<DataContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new DataContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new DataContext(options))
+                {
+                    var group = new Group { Name = "Test Group" };
+                    context.Groups.Add(group);
+                    context.SaveChanges();
+                }
+
+                using (var context = new DataContext(options))
+                {
+                    Group group = await context.Groups.FirstAsync();
+                    Assert.Equal("Test Group", group.Name);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
